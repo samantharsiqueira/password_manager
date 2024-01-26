ab0d375deb86a10013b6e9a4ae132167a12314fd
Samantha Siqueira <samantharsiqueira@gmail.com>
2024-01-26 16:17:13 +0000

    first one

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Title from './components/Title';
+import Form from './components/Form';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Title />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,37 @@
+export default function Form() {
+  return (
+    <form>
+      <label>
+        Nome do serviço
+        <input
+          type="text"
+          name="name"
+        />
+      </label>
+
+      <label>
+        Login
+        <input
+          type="text"
+          name="login"
+        />
+      </label>
+      <label>
+        Senha
+        <input
+          type="text"
+          name="senha"
+        />
+      </label>
+      <label>
+        URL
+        <input
+          type="text"
+          name="url"
+        />
+      </label>
+      <button>Cadastrar</button>
+      <button>Cancelar</button>
+    </form>
+  );
+}
